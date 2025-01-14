0b87657572bc31ba8c9f46c6388f6eb02be78cc5
danie <danie@DANIEL>
2025-01-14 10:43:09 -0300

    Adicionar arquivos de projeto.

---
+++ Banco_de_Dados_Hotel/Dados.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_de_Dados_Hotel
+{
+    internal class Dados
+    {
+        public string Cliente {  get; private set; }
+        public string Email {  get; private set; }
+        public string Cpf {   get; private set; }
+
+        public Dados(string cliente,string email ,string cpf)
+        {
+            Cliente = cliente;
+            Email = email;
+            Cpf = cpf;
+        }
+        public override string ToString() 
+        {
+            return Cliente +"; " + Email;
+        }
+    }
+}
+
+
---
+++ Banco_de_Dados_Hotel/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Http.Json;
+
+namespace Banco_de_Dados_Hotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados[] dados = new Dados[10];
+            Console.Write("Digite a quantidade de Clientes: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            //Coletando Imformaçoes 
+            for(int i = 0; i < quantidade; i++)
+            {
+                string? cliente;
+                string? email;
+                string? cpf;
+                do
+                {
+                    //Coletade Imformaçoes Basicas 
+                    Console.Clear();
+                    Console.WriteLine($"Aluguel #{i + 1}");
+                    Console.Write($"Nome: ");
+                    cliente = Console.ReadLine();
+                    Console.Write("Email: ");
+                    email = Console.ReadLine();
+                    Console.Write("CPF: ");
+                    cpf = (Console.ReadLine());
+                    if(cliente == null || email == null || cpf == null)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Perdecha todos dos topicos !!! ");
+                        Task.Delay(500).Wait();
+                    }
+                } while (cliente == null || email == null || cpf == null);
+                //Verificando Quarto Disponivel 
+                int quarto;
+                do
+                {
+                    Console.Write("Quarto");
+                    quarto = int.Parse(Console.ReadLine());
+                    if(dados[quarto] != null)
+                    {
+                        Console.WriteLine("Quarto ocuapdo escolha outro !!");                                                 
+                    }
+                } while (dados[quarto] != null);
+                //quarto disponivel Emcerindo imformaçoes 
+                dados[quarto] = new Dados(cliente,email,cpf);
+                Console.Clear();
+            }
+            //Printar informaçoes Cadastradas 
+            for(int i = 0; i < dados.Length; i++)
+            {
+                if (dados[i] != null)
+                {
+                    Console.WriteLine($"{i}: " + dados[i]);
+                }
+            }
+        }
+    }
+}
